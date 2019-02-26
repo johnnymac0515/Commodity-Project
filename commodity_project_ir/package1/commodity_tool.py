d766ddad40d90bc7d656b43a7331348c6fd18d65
John Macnamara <john.macnamara5@gmail.com>
2019-02-26 01:30:22 -0500

    Updated commodity_tool.py to accept user command line arg for file path and also load in data as as pandas dataframe

--- commodity_project_ir/package1/commodity_tool.py
+++ commodity_project_ir/package1/commodity_tool.py
@@ -1,17 +1,38 @@
-"""This module runs as a pseudo main function and data set into a dataframe"""
-import commodity_project_ir
-def load_data():
-    """load_data takes no arugments (currently) and loads in the dataset identified in this project.
-    it then calls TODO"""
-    print("load data here")
+"""This module runs as a pseudo main function and data set into a dataframe. It requests the data 
+   path stored locally by the user"""
 
+import pandas as pd
 
-def main():
-    """Runs if this module is imported"""
-    print("Other main ran")
-    load_data()
+def load_data(path):
+    """load_data takes path and loads in the dataset identified in this project into a pandas df.
+     It then prints high level informatio about the dataframe for the users reference and returns 
+     this dataframe"""
+    
+    columns = ['Item Year', 'Original Value', 'Standard Value', 'Original Currency',  
+               'Standard Currency', 'Orignal Measure', 'Standard Measure', 'Location',
+               'Commodity', 'Variety']
+    
+    au_df = pd.read_csv(path, usecols= columns)
 
-if __name__ == 'main':
+    print(au_df.head())
+
+if __name__ == '__main__':
     #Psuedo main function. This construct is the entry point for the tool.
-    print('main ran')
-    load_data()
+    au_path = input("Enter the local file path for the Allen/Unger 'All Commodities' dataset\nPath: ")
+    
+    try:
+        with open(au_path, 'r'): print('found')
+    
+    except FileNotFoundError:
+        print('Could not find file at that location')
+
+    try:
+        au_path[len(au_path)-4:] == '.csv'  
+    
+    except AttributeError:
+        print('Incorrect File type. File must be .csv')
+
+    load_data(au_path)
+
+
+
