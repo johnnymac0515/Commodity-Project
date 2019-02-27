71047bc0c8e7ca100b55cf4f290e49d15fd0ca0c
John Macnamara <john.macnamara5@gmail.com>
2019-02-27 02:19:36 -0500

    created sql_gener file with begginins of SQL database class and support functions, deleted command_line.py as unnecessary at this time.

--- commodity_project_ir/package1/commodity_tool.py
+++ commodity_project_ir/package1/commodity_tool.py
@@ -2,37 +2,55 @@
    path stored locally by the user"""
 
 import pandas as pd
+import sys
 
 def load_data(path):
     """load_data takes path and loads in the dataset identified in this project into a pandas df.
-     It then prints high level informatio about the dataframe for the users reference and returns 
+     It then prints high level information about the dataframe for the users reference and returns 
      this dataframe"""
     
     columns = ['Item Year', 'Original Value', 'Standard Value', 'Original Currency',  
                'Standard Currency', 'Orignal Measure', 'Standard Measure', 'Location',
-               'Commodity', 'Variety']
+               'Commodity']
+    col_type = [int, float, float, object, object, object, object, object]
     
+    col_type_dict = dict(zip(columns, col_type))
+   
     au_df = pd.read_csv(path, usecols= columns)
+    au_df = au_df.astype(col_type_dict)
+    au_df.name = 'AU_data'
+    au_df_trunc = au_df.iloc[1:50]
+    au_df_trunc.to_csv('/Users/johnmacnamara/Desktop/test_data2.csv')
+    return au_df
 
-    print(au_df.head())
 
-if __name__ == '__main__':
-    #Psuedo main function. This construct is the entry point for the tool.
+def main():
+    """Psuedo main function."""
+    # TODO replace below with argparser
+        
     au_path = input("Enter the local file path for the Allen/Unger 'All Commodities' dataset\nPath: ")
     
     try:
-        with open(au_path, 'r'): print('found')
+        with open(au_path, 'r'): pass
     
     except FileNotFoundError:
         print('Could not find file at that location')
 
     try:
-        au_path[len(au_path)-4:] == '.csv'  
-    
+
+        if au_path[len(au_path)-4:] != '.csv':
+            raise AttributeError 
+        
     except AttributeError:
         print('Incorrect File type. File must be .csv')
 
-    load_data(au_path)
+    load_data(au_path) 
+
+if __name__ == '__main__':
+    # This construct is the entry point for the tool.
+    # sys.exit(main())
+    main()
+    
 
 
 
---
+++ commodity_project_ir/package1/sql_gener.py
@@ -0,0 +1,59 @@
+"""This module creates a local SQL 'database' for the tools dataset.""" 
+
+
+import sqlite3
+from functools import reduce
+
+
+def map_dftype_to_sqldtype(df):
+    """Takes (at a minimum 1 row of) DataFrame object and combines the Column Name and dtype of the 
+    DF that and maps them to useable column parameters and SQL types (supported by sqlite)""" 
+
+    data_type_map = {'i':'INTEGER', 'f':'REAL', 'None':'NULL', 'object':'TEXT'}
+
+    df_cols = list(df.columns)
+    
+    df_types_list = [ data_type_map[col].dtype.kind() for col in df_cols ]
+    sql_types_list = [ data_type_map[i] for i in df_types_list ]
+
+    #Now We need to remove white space from column names so that work as SQL Column parameters
+    #A reduce, a lambda, and a list comprehension all in one line....sometime I cant help but impress myself
+    sql_col = [reduce((lambda a, b: a + b), col_name.split(' ')) for col_name in list(df_cols)] 
+
+    #Now we'll create a list of strings where each element is the 'colparam DATATYPE' string
+    sql_table_arg_list = [ "{name} {type}".format(name=sql_col[i], type=sql_types_list[i]) for i in list(range(len(sql_col)))]
+
+    return sql_table_arg_list
+
+# def sql_from_data(df):
+#     """Creates a local sql dataframe using 'sqlite3'. The db is named after the passed dataframe.
+#     i.e. if df.name = 'data_set' then the database is named 'data_set.db'"""
+    
+#     conn = sqlite3.connect("{name}.db".format(name=df.name))
+
+#     cursor = conn.cursor()
+
+#     column_tup = tuple(df.columns())
+
+
+
+
+class sql_database(object):
+
+    def __init__(self, df):
+       self.df = df
+    
+    def __repr__(self):
+        self.name = "SQL database class object for {dfname} dataframe".format(dfname= df.name)
+
+    def create_db(self):
+        conn = sqlite3.connect("{name}.db".format(name=df.name))
+
+        curs = conn.cursor()
+
+    def create_table(self):
+        create_table_state = "CREATE TABLE IF NOT EXISTS au_sql_table {df_column_tuble}" 
+
+
+import pandas as pd; df = pd.read_csv('/Users/johnmacnamara/Desktop/test_data2.csv')
+map_dftype_to_sqldtype(df)
