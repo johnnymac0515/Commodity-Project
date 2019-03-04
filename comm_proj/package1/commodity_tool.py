ab2cdeebb4e4df8e2103a0dd5a2b1bd9608ebd0e
John Macnamara <john.macnamara5@gmail.com>
2019-03-03 21:57:20 -0500

    Cleaned up commodity_tool from linter results

--- comm_proj/package1/commodity_tool.py
+++ comm_proj/package1/commodity_tool.py
@@ -1,22 +1,22 @@
-"""This module runs as a pseudo main function and data set into a dataframe. It requests the data 
+"""This module runs as a pseudo main function and data set into a dataframe. It requests the data
    path stored locally by the user"""
 
-import pandas as pd
 import sys
+import pandas as pd 
 
 def load_data(path):
     """load_data takes path and loads in the dataset identified in this project into a pandas df.
-     It then prints high level information about the dataframe for the users reference and returns 
+     It then prints high level information about the dataframe for the users reference and returns
      this dataframe"""
-    
-    columns = ['Item Year', 'Original Value', 'Standard Value', 'Original Currency',  
+
+    columns = ['Item Year', 'Original Value', 'Standard Value', 'Original Currency',
                'Standard Currency', 'Orignal Measure', 'Standard Measure', 'Location',
                'Commodity']
     col_type = [int, float, float, object, object, object, object, object]
-    
+
     col_type_dict = dict(zip(columns, col_type))
-   
-    au_df = pd.read_csv(path, usecols= columns)
+
+    au_df = pd.read_csv(path, usecols=columns)
     au_df = au_df.astype(col_type_dict)
     au_df.name = 'AU_data'
     au_df_trunc = au_df.iloc[1:50]
@@ -26,31 +26,28 @@ def load_data(path):
 
 def main():
     """Psuedo main function."""
-    # TODO replace below with argparser at some point
-        
-    au_path = input("Enter the local file path for the Allen/Unger 'All Commodities' dataset\nPath: ")
-    
+    #replace below with argparser at some point
+
+    au_path = input("Enter the local file path for the Allen/Unger 'All Commodities' \
+    dataset\nPath: ")
+
     try:
-        with open(au_path, 'r'): pass
-    
+        with open(au_path, 'r'):
+            pass
+
     except FileNotFoundError:
         print('Could not find file at that location')
 
     try:
 
         if au_path[len(au_path)-4:] != '.csv':
-            raise AttributeError 
-        
+            raise AttributeError
+
     except AttributeError:
         print('Incorrect File type. File must be .csv')
 
-    load_data(au_path) 
+    load_data(au_path)
 
 if __name__ == '__main__':
     # This construct is the entry point for the tool.
-    # sys.exit(main())
-    main()
-    
-
-
-
+    sys.exit(main())
